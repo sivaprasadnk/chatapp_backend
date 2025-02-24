83d87916b99b17cd6a7999341c77e4103ada84e5
sivaprasadnk <sivaprasadnk123@gmail.com>
2025-02-24 22:05:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('message', (message) => {
+        console.log(`Received: ${message}`);
+
+        // Broadcast message to all connected clients
+        server.clients.forEach((client) => {
+            if (client !== socket && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    socket.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+console.log('WebSocket server running on ws://localhost:8080');
